2ab2d2ba691a39110fdd7bb11d496ee52c2e79a7
Jesse de Zwart <jessedezwart@live.nl>
2022-11-15 16:07:57 +0100

    build: add generated spring files

---
+++ src/test/java/com/alistats/discorki/DiscorkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alistats.discorki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscorkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
