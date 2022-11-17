e7707c7c97e689ad1b216e92b7278b9b37843e72
Nick Dekker <nick@freshcoders.nl>
2022-11-18 00:35:53 +0100

    fix: convert loop, inclusive pronoun
    Also did some formatting fixes in the description.
    DISCLAIMER: untested 👍

--- src/main/java/com/alistats/discorki/notification/TopDpsNotification.java
+++ src/main/java/com/alistats/discorki/notification/TopDpsNotification.java
@@ -2,6 +2,8 @@ package com.alistats.discorki.notification;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -22,15 +24,17 @@ public class TopDpsNotification extends PostGameNotification implements IPostGam
         List<ParticipantDto> participants = Arrays.asList(match.getInfo().getParticipants());
 
         // Check which summoner got the most damage
-        ParticipantDto topDps = participants.get(0);
-        for (ParticipantDto participant : participants) {
-            if (participant.getTotalDamageDealtToChampions() > topDps.getTotalDamageDealtToChampions()) {
-                topDps = participant;
-            }
-        }
+        ParticipantDto topDps = Collections.max(participants, Comparator.comparing(s -> s.getTotalDamageDealtToChampions()));
 
         // Check if summoner is tracked
         ArrayList<Summoner> summoners = summonerRepo.findByIsTracked(true).get();
+        // TODO: check if it doesn't make more sense to check this:
+        //       Summoner trackedSummoner = summonerRepo.findByPuuid(topDps.getPuuid());
+        //       if (trackedSummoner != null) {
+        //          return new ArrayList<EmbedDto>(Arrays.asList(buildEmbed(match, topDps, trackedSummoner)));
+        //       }
+        // as it will only fetch a single (or zero) summoner from the database, eliminating the need for a loop.
+        // Edge case: bot without Puuid did most damage, so return early?
         for (Summoner summoner : summoners) {
             if (summoner.getPuuid().equals(topDps.getPuuid())) {
                 return new ArrayList<EmbedDto>(Arrays.asList(buildEmbed(match, topDps, summoner)));
@@ -45,19 +49,19 @@ public class TopDpsNotification extends PostGameNotification implements IPostGam
         String queueName = gameConstantService.getQueue(match.getInfo().getQueueId()).getDescription();
 
         EmbedDto embedDto = new EmbedDto();
-        embedDto.setTitle(summoner.getName() + "just got TOP DPS!");
+        embedDto.setTitle(summoner.getName() + "just got top DPS!");
         embedDto.setImage(new ImageDto(imageService.getChampionSplashUrl(participant.getChampionName()).toString()));
         embedDto.setThumbnail(new ThumbnailDto(imageService.getMapUrl(match.getInfo().getMapId()).toString()));
         StringBuilder description = new StringBuilder();
         description .append(summoner.getName())
-                    .append(" just got TOP DPS with **")
+                    .append(" just got top DPS with **")
                     .append(participant.getChampionName())
-                    .append("**. He dealt **")
+                    .append("**. They dealt **")
                     .append(participant.getTotalDamageDealtToChampions())
                     .append("** damage in *")
                     .append(match.getInfo().getGameDuration()/60)
-                    .append("* minutes.")
-                    .append("He finished the game with a kda of **")
+                    .append("* minutes. ")
+                    .append("They finished the game with a KDA of **")
                     .append(participant.getKills())
                     .append("/")
                     .append(participant.getDeaths())
