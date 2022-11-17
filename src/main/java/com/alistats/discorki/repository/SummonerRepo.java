e8b056957a08b96303fe70dc2184c4c1e808ae9d
Jesse de Zwart <jessedezwart@live.nl>
2022-11-17 10:32:28 +0100

    fix: annotate repo

--- src/main/java/com/alistats/discorki/repository/SummonerRepo.java
+++ src/main/java/com/alistats/discorki/repository/SummonerRepo.java
@@ -3,9 +3,11 @@ package com.alistats.discorki.repository;
 import java.util.ArrayList;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alistats.discorki.model.Summoner;
 
+@Repository
 public interface SummonerRepo extends JpaRepository<Summoner, String> {
     public Optional<Summoner> findByName(String name);
     public Optional<ArrayList<Summoner>> findByIsTracked(Boolean isTracked);
