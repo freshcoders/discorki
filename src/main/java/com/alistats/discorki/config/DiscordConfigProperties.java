11966bb1c0a494a0000c1547b835a44d68c90491
Jesse de Zwart <jessedezwart@live.nl>
2022-11-19 20:57:00 +0100

    feat: add config validation

--- src/main/java/com/alistats/discorki/config/DiscordConfigProperties.java
+++ src/main/java/com/alistats/discorki/config/DiscordConfigProperties.java
@@ -1,10 +1,10 @@
 package com.alistats.discorki.config;
 
-import javax.validation.constraints.NotBlank;
-
+import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
 import lombok.Getter;
@@ -14,8 +14,10 @@ import lombok.Setter;
 @Component
 @Getter
 @Setter
+@Validated
 @ConfigurationPropertiesScan
 @ConfigurationProperties(prefix = "discord")
 public class DiscordConfigProperties {
-    @NotBlank private String url;
+    @URL(message = "Webhook url is not set. Please refer to https://discord.com/developers/docs/intro for more information.")
+    private String url;
 }
