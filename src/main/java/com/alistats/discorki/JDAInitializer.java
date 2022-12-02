1411e187eac20826dc98e9c64bf32b57909624e6
Jesse de Zwart <jessedezwart@live.nl>
2022-12-02 12:28:57 +0100

    wip: add message listener

--- src/main/java/com/alistats/discorki/JDAInitializer.java
+++ src/main/java/com/alistats/discorki/JDAInitializer.java
@@ -5,16 +5,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.alistats.discorki.config.DiscordConfigProperties;
+import com.alistats.discorki.listener.MessageListener;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 @Component
 public class JDAInitializer implements CommandLineRunner {
-    @Autowired private DiscordConfigProperties discordConfigProperties;
+    @Autowired
+    private DiscordConfigProperties discordConfigProperties;
+    private final MessageListener messageListener;
+
+    public JDAInitializer(MessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
 
     @Override
     public void run(String... args) {
-        JDABuilder builder = JDABuilder.createDefault(discordConfigProperties.getToken());
-        builder.build();
+        JDA jda = JDABuilder.createDefault(discordConfigProperties.getToken())
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+
+        jda.addEventListener(new MessageListener());
     }
 }
---
+++ src/main/java/com/alistats/discorki/listener/MessageListener.java
@@ -0,0 +1,27 @@
+package com.alistats.discorki.listener;
+
+import org.springframework.stereotype.Service;
+
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+@Service
+public class MessageListener extends ListenerAdapter
+{
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContentDisplay());
+        }
+    }
+}
