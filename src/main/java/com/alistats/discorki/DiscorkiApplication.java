2ec932f866155205a97e491ed5dbc61835cde944
Jesse de Zwart <jessedezwart@live.nl>
2023-01-22 11:11:23 +0100

    Refactor: remove unused command line runner

--- src/main/java/com/alistats/discorki/DiscorkiApplication.java
+++ src/main/java/com/alistats/discorki/DiscorkiApplication.java
@@ -1,6 +1,5 @@
 package com.alistats.discorki;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,12 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
-public class DiscorkiApplication implements CommandLineRunner {
+public class DiscorkiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscorkiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
 	}
 }
