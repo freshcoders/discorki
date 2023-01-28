7a9cc5453274258917c2c855f21b0a0f611ae7b6
Jesse de Zwart <jessedezwart@live.nl>
2023-01-28 16:44:08 +0100

    Style: Remove console log

--- src/main/java/com/alistats/discorki/service/ImageService.java
+++ src/main/java/com/alistats/discorki/service/ImageService.java
@@ -31,7 +31,6 @@ public class ImageService {
             .append("/img/champion/")
             .append(championName)
             .append(".png");
-            System.out.println(str.toString());
         return resolveUrl(str.toString());
     }
 
