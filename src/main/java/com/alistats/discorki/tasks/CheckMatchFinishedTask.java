94d914c50a8ef324d74e008387a81955b098b006
Jesse de Zwart <jessedezwart@live.nl>
2023-02-14 21:47:46 +0100

    Refactor: remove unused import

--- src/main/java/com/alistats/discorki/tasks/CheckMatchFinishedTask.java
+++ src/main/java/com/alistats/discorki/tasks/CheckMatchFinishedTask.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
